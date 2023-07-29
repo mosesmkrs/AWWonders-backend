6ad72d99933fdbac52908dcb88aad8e59168df6b
mosesmkrs <karisamoses392@gmail.com>
2023-07-29 15:42:01 +0300

    hfv

--- api/workouts.js
+++
@@ -1,7 +0,0 @@
-
-const express = require('express');
-const workoutRoutes = require('../routes/workouts');
-const app = express();
-app.use(express.json());
-app.use(workoutRoutes);
-module.exports = app;
